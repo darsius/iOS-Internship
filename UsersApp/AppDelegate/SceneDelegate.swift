34e8756b272697cb817e685cc27b1fb699d9d18d
darsius <p_darius211@yahoo.com>
2024-03-22 12:25:22 +0200

    test commit

--- UsersApp/AppDelegate/SceneDelegate.swift
+++ UsersApp/AppDelegate/SceneDelegate.swift
@@ -23,7 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                     window.rootViewController = loadingViewController
                 }
             }
-            
+            //
             window.makeKeyAndVisible()
             self.window = window
     }
