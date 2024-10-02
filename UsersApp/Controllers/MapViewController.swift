7bbd9ce9c9fdaab65f5a5480f6e990971abbacfe
darsius <p_darius211@yahoo.com>
2024-10-02 19:17:34 +0300

    refactored  mapViewController

--- UsersApp/Controllers/MapViewController.swift
+++ UsersApp/Controllers/MapViewController.swift
@@ -8,17 +8,19 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupMapView()
+        fetchUsers()
+    }
+    
+    private func setupMapView() {
         mapView.delegate = self
         view.backgroundColor = .systemYellow
-        
-        fetchUsers()
     }
     
     private func fetchUsers() {
         UsersManager.shared.getUsers(on: self) { [weak self] users in
-            self?.addAnnotations(users: users)
             self?.users = users
+            self?.addAnnotations(users: users)
         }
     }
     
@@ -31,14 +33,24 @@ class MapViewController: UIViewController {
     
     private func createArrowView() -> UIView {
         let arrowView = UIImageView()
+        arrowView.image = UIImage(systemName: "location.north.fill")
         arrowView.frame = CGRect(x: 12, y: 40, width: 15, height: 15)
         arrowView.transform = CGAffineTransform(rotationAngle: .pi)
-        arrowView.image = UIImage(systemName: "location.north.fill")
         return arrowView
     }
     
-    private func createCustomAnnotationView(with userImageUrl: String?) -> UIView {
+    private func createCustomAnnotationView(for userImageUrl: String?) -> UIView {
         let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        let arrowView = createArrowView()
+        let imageView = createUserImageView(with: userImageUrl)
+        
+        containerView.addSubview(arrowView)
+        containerView.addSubview(imageView)
+        
+        return containerView
+    }
+    
+    private func createUserImageView(with userImageUrl: String?) -> UIView {
         
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
         imageView.layer.cornerRadius = 20;
@@ -61,19 +73,14 @@ class MapViewController: UIViewController {
         } else {
             imageView.image = defaultImage
         }
-        
-        let arrowView = createArrowView()
-        
-        containerView.addSubview(imageView)
-        containerView.addSubview(arrowView)
-        
-        return containerView
+                
+        return imageView
     }
 }
 
 extension MapViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        guard let annotation = annotation as? UserAnnotation else {
+        guard let userAnnotation = annotation as? UserAnnotation else {
             return nil
         }
         
@@ -81,22 +88,27 @@ extension MapViewController: MKMapViewDelegate {
         var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
         
         if annotationView == nil {
-            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-            annotationView?.canShowCallout = true
-            annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            annotationView = MKAnnotationView(annotation: userAnnotation, reuseIdentifier: identifier)
         } else {
-            annotationView?.annotation = annotation
+            annotationView?.annotation = userAnnotation
         }
         
-        if let imageUrl = annotation.imageUrl {
-            let customView = createCustomAnnotationView(with: imageUrl)
-            annotationView?.addSubview(customView)
-            annotationView?.frame = customView.frame
-            
-            return annotationView
+        if let annotationView {
+            configureAnnotationView(annotationView, with: userAnnotation)
         }
         
         return annotationView
+    }
+    
+    private func configureAnnotationView(_ annotationView: MKAnnotationView, with annotation: UserAnnotation) {
+        annotationView.canShowCallout = true
+        annotationView.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        
+        if let imageUrl = annotation.imageUrl {
+            let customView = createCustomAnnotationView(for: imageUrl)
+            annotationView.addSubview(customView)
+            annotationView.frame = customView.frame
+        }
     }
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
